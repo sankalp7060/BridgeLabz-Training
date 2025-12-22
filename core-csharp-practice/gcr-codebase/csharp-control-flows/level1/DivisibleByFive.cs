9babaaaa0090a6ff778cc34e563b75650b9cd65b
Sankalp Agarwal <sankalpagarwal8057@gmail.com>
2025-12-22 15:09:07 +0530

    [Sankalp_Agarwal] Add level 1 of control flow

--- core-csharp-practice/gcr-codebase/csharp-control-flows/level1/DivisibleByFive.cs
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level1/DivisibleByFive.cs
@@ -7,10 +7,10 @@ class DivisibleByFive{
         int num = int.Parse(Console.ReadLine()); //User given number
         //Check the number is divisible by 5 or not
         if( num % 5 == 0){
-            Console.WriteLine("The given number is divisible by 5")
+            Console.WriteLine("The given number is divisible by 5");
         }
         else{
-            Console.WriteLine("The given number is not divisible by 5")
+            Console.WriteLine("The given number is not divisible by 5");
         }
     }
 }
